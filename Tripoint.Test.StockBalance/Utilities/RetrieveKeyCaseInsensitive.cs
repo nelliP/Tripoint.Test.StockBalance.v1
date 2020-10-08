dd850ce28c4bc02bf5abfcdc92495d83d5b79bd9
nellip <co.pfeiffer@gmail.com>
2020-10-08 14:01:49 +0200

    renamed

---
+++ Tripoint.Test.StockBalance/Utilities/RetrieveKeyCaseInsensitive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tripoint.Test.StockBalance.Utilities
+{
+    public static class RetrieveKeyCaseInsensitive
+    {
+       public static string RetrieveKey(string key, Dictionary<string, int> dict)
+        {
+            var comparer = StringComparison.OrdinalIgnoreCase;
+            var retrievedKey = dict.FirstOrDefault(x => String.Equals(x.Key, key, comparer)).Key;
+            return retrievedKey;
+        }        
+    }
+}
