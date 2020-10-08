231053d03c1d989c69aa148f03e95b81f519e504
nellip <co.pfeiffer@gmail.com>
2020-10-08 14:00:37 +0200

    added utility method to retrive key case-insensitive

---
+++ Tripoint.Test.StockBalance/Utilities/RetriveKeyCaseInsensitive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tripoint.Test.StockBalance.Utilities
+{
+    public static class RetriveKeyCaseInsensitive
+    {
+       public static string RetriveKey(string key, Dictionary<string, int> dict)
+        {
+            var comparer = StringComparison.OrdinalIgnoreCase;
+            var retrivedKey = dict.FirstOrDefault(x => String.Equals(x.Key, key, comparer)).Key;
+            return retrivedKey;
+        }        
+    }
+}
